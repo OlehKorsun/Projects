4e9f5634db172acf17817bc47707129252f758ae
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-08 21:21:13 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        MyContainer cont = new MyContainer();
+        MyContainer cont2 = new MyContainer();
+
+        cont.fun(5, 10, 25.5);
+        cont2.fun(20, 30, 40.5);
+        System.out.println(cont.weight);
+    }
+}
---
+++ src/MyContainer.java
@@ -0,0 +1,16 @@
+public class MyContainer {
+    static int weight;
+    int value;
+    double value2;
+
+    public void fun(int a, int b, double c){
+        weight = a;
+        this.value = b;
+        this.value2 = c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+}
