eed76c6863c2fd41b1ee7cd634d4dfc5b5f8b464
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-14 22:08:45 +0100

    Add files via upload

---
+++ Zestaw15Zadanie1/Child.java
@@ -0,0 +1,39 @@
+package Zestaw15Zadanie1;
+
+
+public class Child {
+    private String name;
+    private int numCandies;
+
+    Child(String name){
+        this.name = name;
+        this.numCandies = 2;
+    }
+
+    void givesCandyTo(Child other){
+        if(this.numCandies<1){
+            System.out.println("No candies to give!");
+            return;
+        }
+        this.numCandies--;
+        other.numCandies++;
+    }
+
+    static Child getChildByName(Child[] children, String name){
+        for(int i = 0; i<children.length; i++){
+            if(children[i].name.equals(name))
+                return children[i];
+        }
+
+        return null;
+    }
+
+    static void printChildren(Child[] children){
+        System.out.print("[ ");
+        for(int i = 0; i<children.length; i++){
+            System.out.print("(" + children[i].name + ", " + children[i].numCandies + " candies)");
+        }
+        System.out.println(" ]");
+    }
+
+}
---
+++ Zestaw15Zadanie1/Main.java
@@ -0,0 +1,43 @@
+package Zestaw15Zadanie1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("3 childrens will be created");
+        Child[] tabl = new Child[3];
+
+        for(int i = 0; i<3; i++){
+            System.out.print("Name of the child no " + i + ": " );
+            Scanner scan = new Scanner(System.in);
+            String a = scan.nextLine();
+            tabl[i] = new Child(a);
+        }
+        System.out.print("3 childrens: " );
+        Child.printChildren(tabl);
+        System.out.println("Entering the name of the 'given' starting with 'q' terminates the program");
+
+        System.out.print("Enter names of 'giver' and 'receiver': ");
+        Scanner scan = new Scanner(System.in);
+        String name1 = scan.next();
+        String name2 = scan.next();
+        Child child1;
+        Child child2;
+
+
+        while(!(name1.startsWith("q"))){
+
+            child1 = Child.getChildByName(tabl, name1);
+            child2 = Child.getChildByName(tabl, name2);
+
+            if(child1==null || child2==null)
+                continue;
+
+            child1.givesCandyTo(child2);
+
+        }
+        return;
+
+    }
+}
