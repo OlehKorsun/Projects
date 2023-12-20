823d1ef53e328bec6b5b05bcf89a1a7614f9eb2c
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-20 17:16:13 +0100

    Add files via upload

---
+++ ZestawTomaszewski/Zadanie1/Main.java
@@ -0,0 +1,7 @@
+package ZestawTomaszewski.Zadanie1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MathOperations.calculateCircleArea(10));
+    }
+}
---
+++ ZestawTomaszewski/Zadanie1/MathOperations.java
@@ -0,0 +1,8 @@
+package ZestawTomaszewski.Zadanie1;
+
+public class MathOperations {
+    static final double pi = 3.14;
+    public static double calculateCircleArea(double promien){
+        return pi*promien*promien;
+    }
+}
