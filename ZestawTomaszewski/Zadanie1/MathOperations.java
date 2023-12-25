f1dd466eb07db1d4200f82517e823f667b79ff11
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-25 21:22:42 +0100

    Update MathOperations.java

--- ZestawTomaszewski/Zadanie1/MathOperations.java
+++ ZestawTomaszewski/Zadanie1/MathOperations.java
@@ -2,7 +2,10 @@ package ZestawTomaszewski.Zadanie1;
 
 public class MathOperations {
     static final double pi = 3.14;
+    
     public static double calculateCircleArea(double promien){
+        
         return pi*promien*promien;
+        
     }
 }
