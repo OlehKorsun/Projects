e4286dbfe3ed4350145b4e4dee4137f49c78bb57
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-26 23:18:11 +0100

    Update Gruszka.java

--- Zestaw21TomaszewskiZadanie3/Gruszka.java
+++ Zestaw21TomaszewskiZadanie3/Gruszka.java
@@ -1,7 +1,10 @@
 package Zestaw21TomaszewskiZadanie3;
 
-public class Gruszka extends Owoc{
+public 
+    class Gruszka 
+    extends Owoc{
     public Gruszka(){
+        
         super.setNazwa("Gruszka");
         super.setMasa(Math.random()*150+100);
     }
