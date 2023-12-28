5f7038969731770eebff4bb2262b5c06be51a183
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-28 22:58:28 +0100

    Update Owoc.java

--- Zestaw21TomaszewskiZadanie3/Owoc.java
+++ Zestaw21TomaszewskiZadanie3/Owoc.java
@@ -10,12 +10,15 @@ public
     public void setNazwa(String nazwa){
         this.nazwa = nazwa;
     }
+        
     public String getNazwa(){
         return this.nazwa;
     }
+        
     public void setMasa(double masa){
         this.masa = masa;
     }
+        
     public double getMasa(){
         return this.masa;
     }
