b9c850f8f01ee8c731d1d90084ec5eee25c82eb2
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-28 22:58:50 +0100

    Update Jablko.java

--- Zestaw21TomaszewskiZadanie3/Jablko.java
+++ Zestaw21TomaszewskiZadanie3/Jablko.java
@@ -2,7 +2,9 @@ package Zestaw21TomaszewskiZadanie3;
 
 public class Jablko extends Owoc{
     public Jablko(){
+        
         super.setNazwa("Jablko");
         super.setMasa(Math.random()*150+100);
+        
     }
 }
