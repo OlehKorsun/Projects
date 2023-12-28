5db09618b03063788e73cad01630b15d0cb33894
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-28 22:59:01 +0100

    Update Pomarancza.java

--- Zestaw21TomaszewskiZadanie3/Pomarancza.java
+++ Zestaw21TomaszewskiZadanie3/Pomarancza.java
@@ -2,7 +2,9 @@ package Zestaw21TomaszewskiZadanie3;
 
 public class Pomarancza extends Owoc{
     public Pomarancza(){
+        
         super.setNazwa("Pomarancza");
         super.setMasa(Math.random()*150+100);
+        
     }
 }
