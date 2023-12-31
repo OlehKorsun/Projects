0a5bcbffa4d6d080966fec845c91891e3a4ae6fa
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-31 17:20:30 +0100

    Update Main.java

--- Zestaw21TomaszewskiZadanie4/Main.java
+++ Zestaw21TomaszewskiZadanie4/Main.java
@@ -1,4 +1,16 @@
 package Zestaw21TomaszewskiZadanie4;
 
 public class Main {
+    public static void main(String[] args) throws BlendnaOperacja {
+        Osoba os1 = new Osoba("Katarzyna");
+        Osoba os2 = new Osoba("Michal");
+
+        RachunekBankowy rach1 = new RachunekBankowy(os1, 100);
+        RachunekBankowy rach2 = new RachunekBankowy(os2, 500);
+
+        rach1.przelew(rach2, 100);
+        rach1.wplata(200);
+        System.out.println(rach1);
+        System.out.println(rach2);
+    }
 }
