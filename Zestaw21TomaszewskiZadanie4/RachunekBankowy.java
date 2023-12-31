7b9ebe2a388ea8320828f1df752f6b2c9c877969
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-31 17:20:09 +0100

    Update RachunekBankowy.java

--- Zestaw21TomaszewskiZadanie4/RachunekBankowy.java
+++ Zestaw21TomaszewskiZadanie4/RachunekBankowy.java
@@ -8,15 +8,36 @@ public class RachunekBankowy {
         this.wlasciciel = osoba;
         this.stan = stan;
     }
-    public void wplata(double kwota){
+    public void wplata(double kwota) throws BlendnaOperacja{
+        if(kwota<0)
+            throw new BlendnaOperacja("Zbyt mala kwota");
         this.stan += kwota;
     }
-    public void wyplata(double kwota){
+    public void wyplata(double kwota) throws BlendnaOperacja{
+        if(kwota<0 || kwota>this.stan)
+            throw new BlendnaOperacja("Blad");
         this.stan -= kwota;
     }
 
-    public void przelew(RachunekBankowy rachunek,double kwota){
+    public void przelew(RachunekBankowy rachunek,double kwota) throws BlendnaOperacja{
+//        if(kwota<=0){
+//            throw new BlendnaOperacja("Nieprawidlowa kwota");
+//        }
+//        if(this.stan<kwota){
+//            throw new BlendnaOperacja("Nie wystarcza srodkow na koncie");
+//        }
+        if(kwota<0 || this.stan<kwota)
+            throw new BlendnaOperacja("Blad");
         this.stan -= kwota;
         rachunek.stan += kwota;
+    }
+
+//    void blad(double kwota, double stan) throws BlendnaOperacja{
+//        if(kwota<0 || kwota>stan)
+//            throw new BlendnaOperacja("Blad!");
+//    }
+
+    public String toString() {
+        return "Osoba: " + wlasciciel.name + ", stan: " + this.stan;
     }
 }
