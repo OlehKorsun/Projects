997a630d25d40dd029997d11a1690530b4ef9ef3
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2023-12-31 17:19:23 +0100

    Add files via upload

---
+++ Zestaw21TomaszewskiZadanie4/Konto.java
@@ -0,0 +1,11 @@
+package Zestaw21TomaszewskiZadanie4;
+
+public class Konto extends RachunekBankowy{
+    double oprocentowanie;
+    public Konto(){}
+    public Konto(Osoba osoba, double stan, double procent){
+        super(osoba, stan);
+        this.oprocentowanie = procent;
+    }
+
+}
