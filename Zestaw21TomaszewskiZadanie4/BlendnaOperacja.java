9d50a0f73757f94b1c8fdb3a9192500145fc1237
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-01-01 23:34:51 +0100

    Update BlendnaOperacja.java

--- Zestaw21TomaszewskiZadanie4/BlendnaOperacja.java
+++ Zestaw21TomaszewskiZadanie4/BlendnaOperacja.java
@@ -1,7 +1,7 @@
 package Zestaw21TomaszewskiZadanie4;
 
 public class BlendnaOperacja extends Exception{
-    public BlendnaOperacja(String sms) {
-        super(sms);
+    public BlendnaOperacja(String description) {
+        super(description);
     }
 }
