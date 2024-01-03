f56e695a544e7a8336ce7213689372eaff12ad58
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-01-03 23:33:57 +0100

    Add files via upload

---
+++ Zestaw21TomaszewskiZadanie9/Main.java
@@ -0,0 +1,12 @@
+package Zestaw21TomaszewskiZadanie9;
+
+public class Main {
+    public static void main(String[] args) {
+        ///Osoba os1 = new Osoba("Martin", "Kowalski", "777-777-777");
+        //Osoba os2 = new Osoba("Jakub", "Zaufert", "666-666-666");
+        Osoba os3 = new Osoba("Mateusz", "Wasita", "555-555-555");
+        Smartfon smartfon1 = new Smartfon(null, "blue", "Martin", "Kowalski", "777-777-777");
+        Smartfon smartfon2 = new Smartfon(null, "blue", "Jakub", "Zaufert", "666-666-666");
+        Komorka komorka = new Komorka();
+    }
+}
