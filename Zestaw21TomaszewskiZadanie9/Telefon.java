8808ca81ba64f3e0c554ead80e9b75662d810cd9
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-01-03 23:35:05 +0100

    Add files via upload

---
+++ Zestaw21TomaszewskiZadanie9/Telefon.java
@@ -0,0 +1,17 @@
+package Zestaw21TomaszewskiZadanie9;
+
+public class Telefon {
+    String interfejsKomunikacyjny;
+    String color;
+    public Telefon(){}
+    public Telefon(String interfejs, String color){
+        this.interfejsKomunikacyjny = interfejs;
+        this.color = color;
+    }
+    public void zadzwon(String numer){
+        System.out.println(numer);
+    }
+    public void wyswietHistoriePolaczen(){
+        System.out.println("Brak historii");
+    }
+}
