d3cb80955568ac5554526ad984cd7526e295dc91
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-01-03 23:34:36 +0100

    Add files via upload

---
+++ Zestaw21TomaszewskiZadanie9/Osoba.java
@@ -0,0 +1,13 @@
+package Zestaw21TomaszewskiZadanie9;
+
+public class Osoba {
+    String imie;
+    String nazwisko;
+    String numer;
+    public Osoba(){}
+    public Osoba(String imie, String nazwisko, String numer){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.numer = numer;
+    }
+}
