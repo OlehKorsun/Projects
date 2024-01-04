e8cd78da921d4b0c0b45aa7227d09276b2780a37
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-01-04 22:14:58 +0100

    Add files via upload

---
+++ Zestaw17Zadanie1/Main.java
@@ -0,0 +1,12 @@
+package Zestaw17Zadanie1;
+
+public class Main {
+    public static void main(String[] args) {
+        SortedSLL list = new SortedSLL();
+        list.addSorted(4);
+        list.addSorted(1);
+        list.addSorted(6);
+        list.addSorted(3);
+        list.show();
+    }
+}
