69d6f543db59c043e2444651839956b160a01bbc
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-01-04 22:15:34 +0100

    Add files via upload

---
+++ Zestaw17Zadanie1/Node.java
@@ -0,0 +1,14 @@
+package Zestaw17Zadanie1;
+
+public class Node {
+    int a;
+    Node next;
+    public Node(int a){
+        this.a = a;
+        this.next = null;
+    }
+    public Node(int a, Node next){
+        this.a = a;
+        this.next = next;
+    }
+}
