0d446c56e828f3c42b72d19ac99f5e3a0a40520a
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-01-11 23:58:44 +0100

    Update Komorka.java

--- Zestaw21TomaszewskiZadanie9/Komorka.java
+++ Zestaw21TomaszewskiZadanie9/Komorka.java
@@ -2,7 +2,9 @@ package Zestaw21TomaszewskiZadanie9;
 
 public class Komorka extends Telefon{
     String[] tabl = new String[10];
-    public Komorka(){}
+    public Komorka(){
+        
+    }
     public Komorka(String interfejs, String color){
         super(interfejs, color);
     }
